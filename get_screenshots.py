50d02aa46872d45082f50b91d07e03e06034831b
Mauricio <ozaki@versatushpc.com.br>
2023-09-12 11:53:40 -0300

    Algumas alterações no script

--- get_screenshots.py
+++ get_screenshots.py
@@ -1,54 +1,69 @@
 #!/usr/bin/env python3
-import itertools
-from tools.screenshot import get_screenshots
+
 import logging
 import os
 import sys
+import itertools
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from tools.screenshot import get_screenshots
+
 load_dotenv()
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 HEADERS = {
-    'User-Agent': 'Your User-Agent String',  # Replace with a user agent string
-    'Accept-Language': 'en-US,en;q=0.5',     # Specify the desired language
+    "User-Agent": "Your User-Agent String",  # Replace with a user agent string
+    "Accept-Language": "en-US,en;q=0.5",  # Specify the desired language
 }
-url = os.getenv("SCREENSHOT_LINK")
+URL = os.getenv("SCREENSHOT_LINK")
 
-response = requests.get(url, headers=HEADERS, timeout=5)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
+def main() -> list:
+    response = requests.get(URL, headers=HEADERS, timeout=5)
 
-    main_content_sections = soup.find_all('section', class_='main-content')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    links = []
-    for section in main_content_sections:
-        h2_element_last = section.find('h2', text='Últimas notícias')
-        h2_element_plus = section.find('h2', text='Mais notícias')
+        main_content_sections = soup.find_all("section", class_="main-content")
 
-        if h2_element_last or h2_element_plus:
-            article_links = []
-            article_tags = section.find_all('article', class_='card-post -rowdesktop')
+        links = []
+        for section in main_content_sections:
+            h2_element_last = section.find("h2", text="Últimas notícias")
+            h2_element_plus = section.find("h2", text="Mais notícias")
 
-            for article_tag in article_tags:
-                if article_tag.a and article_tag.a.has_attr('href'):
-                    href = article_tag.a['href']
-                    article_links.append(href)
-                else:
-                    logging.warning("No 'href' attribute found in this article tag.")
+            if h2_element_last or h2_element_plus:
+                article_links = []
+                article_tags = section.find_all(
+                    "article", class_="card-post -rowdesktop"
+                )
 
-            links.append(article_links)
-    
-    links = list(itertools.chain.from_iterable(links))
-    logging.info("Links were collected")
+                for article_tag in article_tags:
+                    if article_tag.a and article_tag.a.has_attr("href"):
+                        href = article_tag.a["href"]
+                        article_links.append(href)
+                    else:
+                        logging.warning(
+                            "No 'href' attribute found in this article tag."
+                        )
 
-else:
-    logging.error("Failed to retrieve the web page. Status code: %s", response.status_code)
+                links.append(article_links)
 
-if links:
-    get_screenshots(links=links)
-else:
-    logging.warning("Links list was empty!")
+        links = list(itertools.chain.from_iterable(links))
+        logging.info("Links were collected")
+
+    else:
+        logging.error(
+            "Failed to retrieve the web page. Status code: %s",
+            response.status_code,
+        )
+
+    if links:
+        get_screenshots(links=links)
+    else:
+        logging.warning("Links list was empty!")
+
+
+if __name__ == "__main__":
+    main()
--- tools/screenshot.py
+++ tools/screenshot.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 import time
 import random
+import logging
+import os
+import sys
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+SCREENSHOT_DIRECTORY = os.getenv("SCREENSHOT_DIRECTORY")
+
 
 def get_screenshots(links: list) -> None:
     for link in links:
-        firefox_options = Options()
-        firefox_options.headless = True
-        firefox_options.add_argument(f'user-agent={USER_AGENT}')
-        driver = webdriver.Firefox(options=firefox_options)
-        driver.set_window_size(800, 600)
-        driver.get(link)
-        name = link.split('/')[-2]
-        driver.save_screenshot(f"{name}.png")
-        driver.quit()
+        try:
+            logging.info("Current link: %s", link)
+            firefox_options = Options()
+            firefox_options.headless = True
+            firefox_options.add_argument(f"user-agent={USER_AGENT}")
+            driver = webdriver.Firefox(options=firefox_options)
+            driver.set_window_size(800, 600)
+            driver.get(link)
+            name = link.split("/")[-2]
+            driver.save_screenshot(
+                os.path.join(SCREENSHOT_DIRECTORY, f"{name}.png")
+            )
+            driver.quit()
 
-        sleep_duration = random.uniform(60, 180)
-        time.sleep(sleep_duration)
+            sleep_duration = random.uniform(60, 180)
+            time.sleep(sleep_duration)
+
+        except Exception as e:
+            logging.error("Error processing link %s: %s", link, str(e))
