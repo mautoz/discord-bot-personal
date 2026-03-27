a74fd1d7714cb394cce754946ebf916ccff99661
Mauricio Ozaki <ozaki@versatushpc.com.br>
2026-03-27 00:18:24 -0300

    Refactor check_bot_status: remove _td bots, fix intents, use unicode emojis

--- check_bot_status.py
+++ check_bot_status.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
+
 import subprocess
 import os
 import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 import asyncio
 import datetime
@@ -13,220 +14,68 @@ TOKEN = os.getenv("DISCORD_TOKEN_HAL")
 SERVER_CHANNEL = int(os.getenv("SERVER_CHANNEL"))
 SERVER_CHANNEL_HAL = int(os.getenv("SERVER_CHANNEL_HAL"))
 MSG_DEFAULT = int(os.getenv("SERVER_DEFAULT_MSG"))
-MSG_DEFAULT_TD = int(os.getenv("SERVER_DEFAULT_MSG_TD"))
 MSG_DEFAULT_HAL = int(os.getenv("SERVER_DEFAULT_MSG_HAL"))
-MSG_DEFAULT_HAL_TD = int(os.getenv("SERVER_DEFAULT_MSG_HAL_TD"))
-MSG_STATUS_C3PO_TD = int(os.getenv("MSG_STATUS_C3PO_TD"))
-MSG_STATUS_C3PO = int(os.getenv("MSG_STATUS_C3PO"))
 
 BOTS = {
-    "IMDB": "imdb_bot.py",
-    "Welcome": "welcome_member.py",
-    "Youtube_Search": "youtube_bot.py",
-    "Youtube_News": "youtube_loop_bot.py",
-    "Tweet_Scraper": "tweet_bot.py",
-    "Games_deals": "games_deals_bot",
-    "Movies_upcoming": "movies_upcoming_bot",
-    "Movies_cast": "movies_cast_bot",
-    "Entertainment_news": "entertainment_news_bot",
-    "Datena": "locate_last_screenshots_bot"
+    "Welcome":           "welcome_member.py",
+    "IMDB":              "imdb_bot.py",
+    "Youtube":           "youtube_bot.py",
+    "Games Deals":       "games_deals_bot.py",
+    "Movies Upcoming":   "movies_upcoming_bot.py",
+    "Entertainment News":"entertainment_news_bot.py",
 }
 
-BOTS_TROPA = {
-    "IMDB": "imdb_bot_td.py",
-    "Welcome": "welcome_member_td.py",
-    "Youtube_Search": "youtube_bot_td.py",
-}
+THUMBNAIL = "https://i0.wp.com/www.setcenas.com.br/wp-content/uploads/2020/07/Terminator-Orion.jpg"
 
 
-def check_process(script_name: str):
-    """
-    If we use command line, 'ps aux | grep <script name>' is
-    useful to find your bot script running. This method follow
-    the same idea!
-    """
+def check_process(script_name: str) -> bool:
     result = subprocess.run(["ps", "aux"], capture_output=True, text=True)
-    process_list = result.stdout.strip().split("\n")
-    for process in process_list:
-        if script_name in process:
-            return True
-    return False
+    return any(script_name in line for line in result.stdout.splitlines())
 
 
-intents = discord.Intents.all()
-client = discord.Client()
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 
-# Skynet
-status_embed = discord.Embed(
-    title="Bots Monitor", description="Checking running bots", color=0x00FF00
-)
-status_embed.set_thumbnail(
-    url="https://i0.wp.com/www.setcenas.com.br/wp-content/uploads/2020/07/Terminator-Orion.jpg"
-)
-status_embed.add_field(name="IMDB", value="Offline")
-status_embed.add_field(name="Welcome", value="Offline")
-status_embed.add_field(name="Youtube_Search", value="Offline")
-status_embed.add_field(name="Youtube_News", value="Offline")
-status_embed.add_field(name="Tweet_Scraper", value="Offline")
-status_embed.add_field(name="Games_deals", value="Offline")
-status_embed.add_field(name="Movies_cast", value="Offline")
-status_embed.add_field(name="Movies_upcoming", value="Offline")
-status_embed.add_field(name="Entertainment_news", value="Offline")
-status_embed.add_field(name="Datena", value="Offline")
-status_embed.add_field(name="Last update", value="Offline")
 
-# Tropa
-status_embed_td = discord.Embed(
-    title="Bots Monitor", description="Checking running bots", color=0x00FF00
-)
-status_embed_td.set_thumbnail(
-    url="https://2.bp.blogspot.com/-AnEJVXe3qGQ/XTuocTDXJ-I/AAAAAAAAGSA/eVdmXqqOuTA-92P1rbAuo7aTdv3pQ67-QCK4BGAYYCw/s1600/tropa_dercy_logo_escuro%2Bcopy.png"
-)
-status_embed_td.add_field(name="IMDB", value="Offline")
-status_embed_td.add_field(name="Welcome", value="Offline")
-status_embed_td.add_field(name="Youtube_Search", value="Offline")
-# status_embed_td.add_field(name="Youtube_News", value="Offline")
-# status_embed_td.add_field(name="Tweet_Scraper", value="Offline")
-# status_embed_td.add_field(name="Games_deals", value="Offline")
-status_embed_td.add_field(name="Last update", value="Offline")
+async def build_embed() -> discord.Embed:
+    embed = discord.Embed(
+        title="🤖 Bots Monitor",
+        description="Status dos bots em tempo real",
+        color=discord.Color.blurple(),
+    )
+    embed.set_thumbnail(url=THUMBNAIL)
 
-# Telegram
-status_embed_telegram = discord.Embed(
-    title="Bots Monitor", description="Checking running bots", color=0x00FF00
-)
-status_embed_telegram.set_thumbnail(
-    url="https://static.wikia.nocookie.net/murderseries/images/e/ee/C_3PO.jpg"
-)
-status_embed_telegram.add_field(
-    name="C-3PO Voice Transcription", value="Offline"
-)
-status_embed_telegram.add_field(name="Last update", value="Offline")
+    for name, script in BOTS.items():
+        status = "🟢 Online" if check_process(script) else "🔴 Offline"
+        embed.add_field(name=name, value=status, inline=True)
+
+    last_update = datetime.datetime.now(pytz.timezone("America/Sao_Paulo"))
+    embed.set_footer(text=f"Última atualização: {last_update.strftime('%d/%m/%Y %H:%M:%S')}")
+    return embed
 
 
 async def update_status():
-    """
-    Keep an embed with the status of all current bots.
-    """
-    green_ball = discord.utils.get(client.emojis, name="green_circle")
-    red_ball = discord.utils.get(client.emojis, name="red_circle")
+    embed = await build_embed()
 
-    for i, (name, script) in enumerate(BOTS.items()):
-        status = (
-            f"{green_ball} Online"
-            if check_process(script)
-            else f"{red_ball} Offline"
-        )
-        status_embed.set_field_at(i, name=name, value=status, inline=False)
-
-    last_update = datetime.datetime.now(pytz.timezone("America/Sao_Paulo"))
-    status_embed.set_field_at(
-        i + 1, name="Last update", value=str(last_update), inline=False
-    )
-
-    # Skynet Server
-    channel = client.get_channel(SERVER_CHANNEL)
-    # Check if the embed already exists!
-    status_message = await channel.fetch_message(MSG_DEFAULT)
-    if status_message:
-        await status_message.edit(embed=status_embed)
-    else:
-        await channel.send(embed=status_embed)
-
-    # Tropa Dercy Server
-    channel_skynet = client.get_channel(SERVER_CHANNEL_HAL)
-    # Check if the embed already exists!
-    status_message = await channel_skynet.fetch_message(MSG_DEFAULT_HAL)
-    if status_message:
-        await status_message.edit(embed=status_embed)
-    else:
-        await channel_skynet.send(embed=status_embed)
-
-
-async def update_status_tropa():
-    """
-    Keep an embed with the status of all current bots.
-    """
-    green_ball = discord.utils.get(client.emojis, name="green_circle")
-    red_ball = discord.utils.get(client.emojis, name="red_circle")
-
-    for i, (name, script) in enumerate(BOTS_TROPA.items()):
-        status = (
-            f"{green_ball} Online"
-            if check_process(script)
-            else f"{red_ball} Offline"
-        )
-        status_embed_td.set_field_at(i, name=name, value=status, inline=False)
-
-    last_update = datetime.datetime.now(pytz.timezone("America/Sao_Paulo"))
-    status_embed_td.set_field_at(
-        i + 1, name="Last update", value=str(last_update), inline=False
-    )
-
-    # Skynet Server
-    channel = client.get_channel(SERVER_CHANNEL)
-    # Check if the embed already exists!
-    status_message = await channel.fetch_message(MSG_DEFAULT_TD)
-    if status_message:
-        await status_message.edit(embed=status_embed_td)
-    else:
-        await channel.send(embed=status_embed_td)
-
-    # Tropa Dercy Server
-    channel = client.get_channel(SERVER_CHANNEL_HAL)
-    # Check if the embed already exists!
-    status_message = await channel.fetch_message(MSG_DEFAULT_HAL_TD)
-    if status_message:
-        await status_message.edit(embed=status_embed_td)
-    else:
-        await channel.send(embed=status_embed_td)
-
-
-async def update_status_telegram():
-    """
-    Keep an embed with the status of all current bots.
-    """
-    green_ball = discord.utils.get(client.emojis, name="green_circle")
-    red_ball = discord.utils.get(client.emojis, name="red_circle")
-
-    status = (
-        f"{green_ball} Online"
-        if check_process("voice_transcription_bot.py")
-        else f"{red_ball} Offline"
-    )
-    status_embed_telegram.set_field_at(
-        0, name="C-3PO Voice Transcription", value=status, inline=False
-    )
-
-    last_update = datetime.datetime.now(pytz.timezone("America/Sao_Paulo"))
-    status_embed_telegram.set_field_at(
-        1, name="Last update", value=str(last_update), inline=False
-    )
-
-    # Skynet Server
-    channel = client.get_channel(SERVER_CHANNEL)
-    # Check if the embed already exists!
-    status_message = await channel.fetch_message(MSG_STATUS_C3PO)
-    if status_message:
-        await status_message.edit(embed=status_embed_telegram)
-    else:
-        await channel.send(embed=status_embed_telegram)
-
-    # Tropa Dercy Server
-    channel_skynet = client.get_channel(SERVER_CHANNEL_HAL)
-    # Check if the embed already exists!
-    status_message = await channel_skynet.fetch_message(MSG_STATUS_C3PO_TD)
-    if status_message:
-        await status_message.edit(embed=status_embed_telegram)
-    else:
-        await channel_skynet.send(embed=status_embed_telegram)
+    for channel_id, msg_id in (
+        (SERVER_CHANNEL, MSG_DEFAULT),
+        (SERVER_CHANNEL_HAL, MSG_DEFAULT_HAL),
+    ):
+        channel = client.get_channel(channel_id)
+        if channel is None:
+            continue
+        try:
+            msg = await channel.fetch_message(msg_id)
+            await msg.edit(embed=embed)
+        except discord.NotFound:
+            await channel.send(embed=embed)
 
 
 @client.event
 async def on_ready():
+    print(f"Bot monitor conectado como {client.user}")
     while True:
         await update_status()
-        await update_status_tropa()
-        await update_status_telegram()
         await asyncio.sleep(3600)
 
 
